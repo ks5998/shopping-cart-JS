463b8d830782643a6531cc1c766022a2ca363f2f
Kundan Singh <kundasin591998@outlook.com>
2022-06-03 18:02:50 +0530

    first

---
+++ data.js
@@ -0,0 +1,74 @@
+let shopItemsData = [{
+    id:"one",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+
+},{
+    id:"two",
+    name: "Office Shirt",
+    price: 80,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+},{
+    id:"three",
+    name: "T Shirt",
+    price: 26,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+},{
+    id:"four",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+},{
+    id:"five",
+    name: "Mens Tie",
+    price: 66,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-5.png",
+},{
+    id:"six",
+    name: "Hippy Sneaker",
+    price: 450,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-6.png",
+},{
+    id:"seven",
+    name: "Cobra Blazer",
+    price: 300,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-7.png",
+},{
+    id:"eight",
+    name: "Denim Shirt",
+    price: 150,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-9.png",
+},{
+    id:"nine",
+    name: "Combo Denim",
+    price: 300,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-10.png",
+},{
+    id:"ten",
+    name: "LV Cap",
+    price: 99,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-11.png",
+},{
+    id:"eleven",
+    name: "Armani Shoe",
+    price: 850,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-12.png",
+},{
+    id:"twelve",
+    name: "Blue T-Shirt",
+    price: 85,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img: "images/img-8.png",
+}]
